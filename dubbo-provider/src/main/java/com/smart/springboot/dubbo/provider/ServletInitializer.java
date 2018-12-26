55c4faf62dc354a980d2456f845566fc6e73ce2a
smart <smart@smartdeMacBook-Pro.local>
2018-12-26 14:33:49 +0800

    Initial commit

---
+++ dubbo-provider/src/main/java/com/smart/springboot/dubbo/provider/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.smart.springboot.dubbo.provider;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DubboProviderApplication.class);
+    }
+
+}
+
